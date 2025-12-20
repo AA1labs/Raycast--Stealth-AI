67533f1061bc79cec6c3be756e5acd5fa97f0dcd
Ahmed Albialy <ahmedmoaness@gmail.com>
2025-12-20 14:21:10 +0000

    First release

---
+++ raycast-env.d.ts
@@ -0,0 +1,101 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `action-1` command */
+  export type Action1 = ExtensionPreferences & {
+  /** Action Title - Name shown in notifications */
+  "title": string,
+  /** AI Prompt - Default prompt */
+  "prompt": string
+}
+  /** Preferences accessible in the `action-2` command */
+  export type Action2 = ExtensionPreferences & {
+  /** Action Title - Name shown in notifications */
+  "title": string,
+  /** AI Prompt - Default prompt */
+  "prompt": string
+}
+  /** Preferences accessible in the `action-3` command */
+  export type Action3 = ExtensionPreferences & {
+  /** Action Title - Name shown in notifications */
+  "title": string,
+  /** AI Prompt - Default prompt */
+  "prompt": string
+}
+  /** Preferences accessible in the `action-4` command */
+  export type Action4 = ExtensionPreferences & {
+  /** Action Title - Name shown in notifications */
+  "title": string,
+  /** AI Prompt - Default prompt */
+  "prompt": string
+}
+  /** Preferences accessible in the `action-5` command */
+  export type Action5 = ExtensionPreferences & {
+  /** Action Title - Name shown in notifications */
+  "title": string,
+  /** AI Prompt - Default prompt */
+  "prompt": string
+}
+  /** Preferences accessible in the `action-6` command */
+  export type Action6 = ExtensionPreferences & {
+  /** Action Title - Name shown in notifications */
+  "title": string,
+  /** AI Prompt - The prompt. */
+  "prompt": string
+}
+  /** Preferences accessible in the `action-7` command */
+  export type Action7 = ExtensionPreferences & {
+  /** Action Title - Name shown in notifications */
+  "title": string,
+  /** AI Prompt - The prompt. */
+  "prompt": string
+}
+  /** Preferences accessible in the `action-8` command */
+  export type Action8 = ExtensionPreferences & {
+  /** Action Title - Name shown in notifications */
+  "title": string,
+  /** AI Prompt - The prompt. */
+  "prompt": string
+}
+  /** Preferences accessible in the `action-9` command */
+  export type Action9 = ExtensionPreferences & {
+  /** Action Title - Name shown in notifications */
+  "title": string,
+  /** AI Prompt - The prompt. */
+  "prompt": string
+}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `action-1` command */
+  export type Action1 = {}
+  /** Arguments passed to the `action-2` command */
+  export type Action2 = {}
+  /** Arguments passed to the `action-3` command */
+  export type Action3 = {}
+  /** Arguments passed to the `action-4` command */
+  export type Action4 = {}
+  /** Arguments passed to the `action-5` command */
+  export type Action5 = {}
+  /** Arguments passed to the `action-6` command */
+  export type Action6 = {}
+  /** Arguments passed to the `action-7` command */
+  export type Action7 = {}
+  /** Arguments passed to the `action-8` command */
+  export type Action8 = {}
+  /** Arguments passed to the `action-9` command */
+  export type Action9 = {}
+}
+
